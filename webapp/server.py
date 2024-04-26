10a9e5a3dfeb900a308529d92c94225bfcbb2137
Peter Olsen <olse0321@umn.edu>
2024-04-26 12:49:55 -0500

    Added login endpoints

--- webapp/server.py
+++ webapp/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, send_from_directory, request, session
+from flask import Flask, send_file, send_from_directory, request, session, jsonify
 import os
 import psycopg2
 from psycopg2 import pool
@@ -45,17 +45,34 @@ def serve_static(filename):
 APP APIs -------------------------------------------------------------------------------------------------------------------------------------
 
 '''
-@app.route('/newUser', methods=['POST'])
-def new_user():
+#bcrypt.checkpw(password.encode('utf-8'), res.encode('utf-8'))
+
+@app.route('/validate-login', methods=['POST'])
+def validate_login():
+    body = request.json
+    password = body['password']
+    username = body['username']
+    names = query("SELECT * FROM users WHERE username = %s", (username,))
+    for name in names:
+        if (bcrypt.checkpw(password.encode('utf-8'), name[2].encode('utf-8'))):
+            return jsonify({'status':'success'})
+    return jsonify({'status':'failure'})
+
+
+@app.route('/addUser', methods=['POST'])
+def add_user():
     body =  request.json
     password = body['password']
     username = body['username']
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     query("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashed_password.decode('utf-8')))
-    #bcrypt.checkpw(password.encode('utf-8'), res.encode('utf-8'))
+    return jsonify({'status':'success'})
 
-
-
+@app.route('/userOverlap')
+def user_overlap():
+    name = request.args.get('username')
+    result = query("SELECT * FROM users WHERE username = %s", (name,))
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
