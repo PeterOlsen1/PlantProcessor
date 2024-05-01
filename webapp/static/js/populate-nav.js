56e54b461a7f37d579bc3c010d752c66cf83ce1d
Peter Olsen <olse0321@umn.edu>
2024-05-01 18:56:35 -0500

    Changes to links

--- webapp/static/js/populate-nav.js
+++ webapp/static/js/populate-nav.js
@@ -3,10 +3,10 @@ const nav = document.querySelector('nav');
 
 nav.innerHTML = `
     <div id="navigation-image">
-        <a href="index.html"><img src="/static/views/plant-favicon.png" width="40" height="40" title="Home"></a>
+        <a href="/index"><img src="/views/plant-favicon.png" width="40" height="40" title="Home"></a>
     </div>
     <div id="navigation-links">
-        <a href="/static/html/aboutMe.html">About Me</a> <a href="/static/html/myPlants.html">My Plants</a> <a href="/static/html/addPlant.html">Add a Plant</a>
+        <a href="/aboutMe">About Me</a> <a href="/static/html/myPlants.html">My Plants</a> <a href="/addPlant">Add a Plant</a>
     </div>
 `;
 
