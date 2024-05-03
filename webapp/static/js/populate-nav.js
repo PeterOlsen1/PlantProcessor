8a24bd1b47877586504156014d23bafb2c3f8f72
Peter Olsen <olse0321@umn.edu>
2024-05-02 19:01:34 -0500

    Changed links to reflect new endpoints

--- webapp/static/js/populate-nav.js
+++ webapp/static/js/populate-nav.js
@@ -5,8 +5,15 @@ nav.innerHTML = `
     <div id="navigation-image">
         <a href="/index"><img src="/views/plant-favicon.png" width="40" height="40" title="Home"></a>
     </div>
+    <div id="navigation-image">
+        <a><img src="/views/logout.webp" width="40" height="40" title="Logout" style='left: 5px; top: 10px' onclick='logout()'></a>
+    </div>
     <div id="navigation-links">
-        <a href="/aboutMe">About Me</a> <a href="/static/html/myPlants.html">My Plants</a> <a href="/addPlant">Add a Plant</a>
+        <a href="/aboutMe">About Me</a> <a href="/templates/myPlants">My Plants</a> <a href="/addPlant">Add a Plant</a>
     </div>
 `;
 
+function logout() {
+    fetch('/api/logout');
+    window.location.href="/"
+}
