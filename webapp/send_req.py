a4a8119e2f789d386ba2c667752a102f3d8681a7
Peter Olsen <olse0321@umn.edu>
2024-05-08 12:10:38 -0500

    Testing requests

--- webapp/send_req.py
+++ webapp/send_req.py
@@ -1,4 +1,18 @@
-import requests as r
+import requests
 
-x = r.get('http://localhost:5000/userOverlap?username=peter')
-print(x)
+# Define the request payload (body)
+payload = {
+    "username": "peter",
+    "password": 1
+}
+
+# Define the request headers
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Send the POST request
+response = requests.post('http://127.0.0.1:5000/api/login', json=payload, headers=headers)
+
+# Print the response
+print(response)
